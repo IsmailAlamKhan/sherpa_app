58fde3c723a7b34f65466f794a1748d43996b563
mathmetal <4429609+4amvim@users.noreply.github.com>
2021-05-05 18:39:00 +0530

    FIX | as

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,11 +4,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'models/todo.dart';
+
 class Utils {
   static void showSnackBar(BuildContext context, String text) =>
       Scaffold.of(context);
-        //..ScaffoldMessenger.removeCurrentSnackBar()
-       // ..ScaffoldMessenger.showSnackBar(SnackBar(content: Text(text)));
+  //..ScaffoldMessenger.removeCurrentSnackBar()
+  // ..ScaffoldMessenger.showSnackBar(SnackBar(content: Text(text)));
 
   static DateTime? toDateTime(Timestamp? value) {
     if (value == null) return null;
@@ -26,8 +28,30 @@ class Utils {
           T Function(Map<String, dynamic> json) fromJson) =>
       StreamTransformer<QuerySnapshot, List<T>>.fromHandlers(
         handleData: (QuerySnapshot data, EventSink<List<T>> sink) {
-          final snaps = data.docs.map((doc) => doc.data()).toList();
-          final objects = snaps.map((json) => fromJson(json as Map<String, dynamic>)).toList();
+          final List<Object?> snaps =
+              data.docs.map((doc) => doc.data()).toList();
+
+          final List<T> objects = snaps
+              .map((json) => fromJson(json as Map<String, dynamic>))
+              .toList();
+
+          sink.add(objects);
+        },
+      );
+
+  static StreamTransformer simpletran(
+          Todo Function(Map<String, dynamic> json) fromJson) =>
+      StreamTransformer<QuerySnapshot<Map<String, dynamic>>,
+          List<Todo>>.fromHandlers(
+        handleData: (QuerySnapshot data, EventSink<List<Todo>> sink) {
+          final List<Map<String, dynamic>> snaps = data.docs
+              .map((doc) => doc.data() as Map<String, dynamic>)
+              .toList();
+
+          //* We need to convert the [Object?] into Map<String, dynamic> somehow
+
+          final List<Todo> objects =
+              snaps.map((json) => fromJson(json)).toList();
 
           sink.add(objects);
         },
